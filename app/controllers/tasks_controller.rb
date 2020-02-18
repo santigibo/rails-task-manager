76d2db99a3f9f55f345ff2c3157fff024f231af0
Santiago Giraldo <santigibo@hotmail.com>
2020-02-18 12:48:28 -0500

    Created the file to the page to have some funcionalities. I can now see the list with the tasks path, I can see each task and go back with the task/:id path and can create another task with tasks/new and with two methods in the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    t = Task.create(task_params)
+
+    redirect_to show_path(t.id)
+  end
+
+  private
+
+  # returns a hash of safe params
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
